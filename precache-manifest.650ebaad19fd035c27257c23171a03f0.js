5ef404ffe64edee41ae1ef9f21f8458ccadece97
Twins corporation <etwincorp@gmail.com>
2022-02-02 16:48:23 +0100

    Updates

---
+++ precache-manifest.650ebaad19fd035c27257c23171a03f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c2e90b98a140858d9260ade0df9add",
+    "url": "/index.html"
+  },
+  {
+    "revision": "007051495a74dabd6075",
+    "url": "/static/css/main.4bbf8d27.chunk.css"
+  },
+  {
+    "revision": "44e46e575bd15e35a410",
+    "url": "/static/js/2.d99259c2.chunk.js"
+  },
+  {
+    "revision": "007051495a74dabd6075",
+    "url": "/static/js/main.b3b42258.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c3e8e0a436f85082a847136373f54df5",
+    "url": "/static/media/logo.c3e8e0a4.png"
+  }
+]);
