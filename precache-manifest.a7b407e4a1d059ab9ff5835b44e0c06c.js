4cb0025a9f38ea294d63259d4724b87ae6875b66
DamienLopvet <damienlopvet@gmail.com>
2023-03-09 10:38:03 +0100

    add message bubbles responsive

---
+++ precache-manifest.a7b407e4a1d059ab9ff5835b44e0c06c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4535884f30a2f317e89bd83166c7955",
+    "url": "/frontestG/index.html"
+  },
+  {
+    "revision": "c58c8d99acde8f24407f",
+    "url": "/frontestG/static/css/main.03cf29b7.chunk.css"
+  },
+  {
+    "revision": "77e4e2ddf682b32a49af",
+    "url": "/frontestG/static/js/2.23615d4e.chunk.js"
+  },
+  {
+    "revision": "ce4c6d28b8cdc9e09fe69e64516fb561",
+    "url": "/frontestG/static/js/2.23615d4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c58c8d99acde8f24407f",
+    "url": "/frontestG/static/js/main.910b2022.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/frontestG/static/js/main.910b2022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca21befb9d26fdab4b91",
+    "url": "/frontestG/static/js/runtime-main.abd4e3a2.js"
+  },
+  {
+    "revision": "c3e8e0a436f85082a847136373f54df5",
+    "url": "/frontestG/static/media/logo.c3e8e0a4.png"
+  }
+]);
